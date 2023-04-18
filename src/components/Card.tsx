fab19e115a9c14f5bc68bc405a283af6bd0243f4
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-18 10:13:17 +0700

    adding dependencies

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Card: FC = () => {
+  return (
+    <div>
+      <h1>Card Page</h1>
+    </div>
+  );
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Navbar: FC = () => {
+  return (
+    <div>
+      <h1>Navbar Page</h1>
+    </div>
+  );
+};
