bb870dab1831c559cd3445dc47e3d601ee1bc18e
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-28 16:45:44 +0700

    swagger detail event

--- src/pages/DetailEvent.tsx
+++ src/pages/DetailEvent.tsx
@@ -1,66 +1,215 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Layout } from "../components/Layout";
 import { CardAttandance, CardComment, CardTicket } from "../components/Card";
 import { Input } from "../components/Input";
 import { ButtonAction } from "../components/Button";
 import { BiMap, BiTime, BiTimer } from "react-icons/bi";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useParams } from "react-router-dom";
+import { types } from "util";
+
+interface DetailDataType {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  address: string;
+  details: string;
+  date: string;
+  time: string;
+  location: string;
+  quota: number;
+  duration: number;
+  ticket: string;
+  price: number;
+  image: any;
+  hosted_by: string;
+  participants: [
+    {
+      name: string;
+      image: string;
+    }
+  ];
+  types: [
+    {
+      type_id: number;
+      type_name: string;
+      price: number;
+    }
+  ];
+  comments: [
+    {
+      name: string;
+      image: string;
+      comment: string;
+    }
+  ];
+}
+
+interface objAddType {
+  event_id: number;
+  comment: string;
+}
 
 const DetailEvent: FC = () => {
-  const today = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: false,
-  };
-  const fullDate = today.toLocaleDateString("en-US", options);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<Partial<DetailDataType>>({});
+  const [csrf, setCsrf] = useState<string>("");
+  const params = useParams();
+  const { id } = params;
+  const [objAdd, setObjAdd] = useState<objAddType>({
+    event_id: Number(id),
+    comment: "",
+  });
 
-  const handleCheckout = () => {
-    alert("checkout oke");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get(`/events/${id}`)
+      .then((response) => {
+        const { data } = response.data;
+        setData(data.data);
+        setCsrf(data.csrf);
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: error,
+          showCancelButton: false,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
+  const handleAddComment = () => {
+    axios
+      .post(`/comments`, objAdd)
+      .then((response) => {
+        const { message, code } = response.data;
+        Swal.fire({
+          icon: "success",
+          title: code,
+          text: message,
+          showCancelButton: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            objAdd.comment = "";
+          }
+        });
+      })
+      .catch((error) => {
+        const { message } = error.message;
+        // console.log(message);
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: error,
+          showCancelButton: false,
+        });
+      });
+  };
+
+  let dateStringHeader = "";
+  let dateString = "";
+  let timeString = "";
+
+  if (data.date) {
+    const Newdate = new Date(data.date);
+    const optionsHeader = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour12: false,
+    } as Intl.DateTimeFormatOptions;
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour12: false,
+    } as Intl.DateTimeFormatOptions;
+    dateStringHeader = Newdate.toLocaleDateString("en-US", optionsHeader);
+    dateString = Newdate.toLocaleDateString("en-US", options);
+    timeString = Newdate.toLocaleTimeString(); // format: 5:17:02 PM
+  }
+
+  const handleToCart = (type_id: number) => {
+    console.log(type_id);
+    axios
+      .post(`/transactions/cart`, {
+        type_id: type_id,
+      })
+      .then((response) => {
+        const { message, code } = response.data;
+        Swal.fire({
+          icon: "success",
+          title: code,
+          text: message,
+          showCancelButton: false,
+        });
+      })
+      .catch((error) => {
+        const { message } = error.message;
+        // console.log(message);
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: error,
+          showCancelButton: false,
+        });
+      });
+  };
+
+  console.log(data);
 
   return (
     <Layout>
       <div className="h-full">
         <div className="grid lg:grid-cols-2 ">
           <div className="p-10">
-            <div className="bg-[url('/header3.jpg')] bg-center bg-cover rounded-3xl h-96"></div>
-            <h1 className=" text-xl sm:text-2xl font-semibold text-orange-500 py-5 pr-5 flex justify-end">
+            <div
+              className={`bg-[url('/header3.jpg')] bg-center bg-cover rounded-3xl h-96`}
+            ></div>
+            <h1 className="text-xl sm:text-2xl font-semibold text-orange-500 py-5 pr-5 flex justify-end">
               5 left
             </h1>
-            <p>
-              In this workshop, learn how to optimize your users' digital
-              journeys with hands-on, guided examples of effective in-app
-              programs. You'll learn what user and customer milestones are
-              critical to proving your ROI, how to drive adoption and engagement
-              with key features, and how to tie your product-driven programs to
-              larger business objectives and end-to-end customer experiences.
-            </p>
-            <h1 className="text-xl font-semibold py-10">Hosted By : Irwan</h1>
+            <p className="text-lg">{data.details}</p>
+            <h1 className="text-xl font-semibold py-10">
+              Hosted By : {data.hosted_by}
+            </h1>
           </div>
           <div className="bg-white rounded-tl-@yes p-10 flex flex-col gap-8">
             <div className="flex space-x-2 items-center bg-black p-5 rounded-2xl">
               <BiMap className="text-orange-500 text-2xl" />
-              <h1 className="text-xl xl:text-2xl font-semibold">{"Senayan"}</h1>
+              <h1 className="text-xl xl:text-2xl font-semibold">
+                {data.location}
+              </h1>
             </div>
             <div className="flex space-x-2 items-center bg-black p-5 rounded-2xl">
               <div>
-                {/* <div>{today.toString()}</div> */}
                 <div className="text-xl xl:text-2xl font-semibold flex flex-col gap-5">
                   <div className="flex space-x-3 items-center">
                     <BiTime className="text-orange-500 text-2xl" />
-                    <h1 className="text-xl xl:text-2xl font-semibold">
-                      {fullDate}
+                    <h1 className="text-xl xl:text-2xl font-semibold flex justify-between">
+                      {dateString} at {timeString}
                     </h1>
                   </div>
                   <div className="flex space-x-3 items-center">
                     <BiTimer className="text-orange-500 text-2xl" />
                     <h1 className="text-xl xl:text-2xl font-semibold">
-                      Duration: {"1 hour"}
+                      Duration:{" "}
+                      {data.duration !== undefined
+                        ? `${data.duration} ${
+                            data.duration <= 1 ? "hour" : "hours"
+                          }`
+                        : ""}
                     </h1>
                   </div>
                 </div>
@@ -71,21 +220,15 @@ const DetailEvent: FC = () => {
                 <h1 className="text-2xl font-semibold">Get Ticket</h1>
               </div>
               <div className="p-12 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-5 xl:gap-10">
-                <CardTicket
-                  ticket={"VIP"}
-                  price={"Rp 150.000"}
-                  onClick={() => handleCheckout()}
-                />
-                <CardTicket
-                  ticket={"Reguler"}
-                  price={"Rp 100.000"}
-                  onClick={() => handleCheckout()}
-                />
-                <CardTicket
-                  ticket={"Free"}
-                  price={"Rp 0"}
-                  onClick={() => handleCheckout()}
-                />
+                {data.types &&
+                  data.types.map((type) => (
+                    <CardTicket
+                      key={type.type_id}
+                      ticket={type.type_name}
+                      price={type.price}
+                      onClick={() => handleToCart(type.type_id)}
+                    />
+                  ))}
               </div>
             </div>
           </div>
@@ -93,13 +236,18 @@ const DetailEvent: FC = () => {
         <div className="grid lg:grid-cols-2 ">
           <div className="px-10 pb-10">
             <div>
-              <h1 className="text-xl font-semibold py-5">Attandance : 5</h1>
+              <h1 className="text-xl font-semibold py-5">
+                Attandance : {data.participants?.length}
+              </h1>
               <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-5">
-                <CardAttandance image="/avatar.jpg" name="irwan" />
-                <CardAttandance image="/avatar.jpg" name="irwan" />
-                <CardAttandance image="/avatar.jpg" name="irwan" />
-                <CardAttandance image="/avatar.jpg" name="irwan" />
-                <CardAttandance image="/avatar.jpg" name="irwan" />
+                {data.participants &&
+                  data.participants.map((data, index) => (
+                    <CardAttandance
+                      key={index}
+                      image={data.image}
+                      name={data.name}
+                    />
+                  ))}
                 <CardAttandance image="/avatar.jpg" name="irwan" />
                 <CardAttandance image="/avatar.jpg" name="irwan" />
               </div>
@@ -107,16 +255,26 @@ const DetailEvent: FC = () => {
             <div className="py-10">
               <h1 className="text-xl font-semibold py-5">Comment</h1>
               <div className="flex space-x-3">
-                <Input />
-                <ButtonAction label="Add" />
+                <Input
+                  placeholder="Enter Comment"
+                  id="input-comment"
+                  type="text"
+                  onChange={(event) =>
+                    setObjAdd({ ...objAdd, comment: event.target.value })
+                  }
+                />
+                <ButtonAction label="Add" onClick={() => handleAddComment()} />
               </div>
             </div>
             <div className="grid grid-flow-row gap-5 border-2 rounded-2xl p-5">
-              <CardComment
-                image={"/avatar.jpg"}
-                name={"Irwan"}
-                comment={"apakah event nya bagus?"}
-              />
+              {data.comments &&
+                data.comments.map((item) => (
+                  <CardComment
+                    image={`/${item.image}`}
+                    name={item.name}
+                    comment={item.comment}
+                  />
+                ))}
               <CardComment
                 image={"/avatar.jpg"}
                 name={"Wibowo"}
